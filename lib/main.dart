92a2a12a631db696b35d07809d9164c078a81af5
JavlonDjamaldinov <javlondjamaldinov@gmail.come>
2024-02-19 15:38:46 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:coffee_ui_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xFF212325)),
+      home: WelcomeScreen(),
+    );
+  }
+}
